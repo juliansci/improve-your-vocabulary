bbb47260bc65a4fc24f4ab7aa519edb1940da501
Julián Scialabba <julian.scialabba@gmail.com>
2020-07-26 17:30:04 -0300

    Updates

---
+++ precache-manifest.756ca7f2f6b7267810961a251848cd4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8b3cdc0274b18b9962a8eb906bf3f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea9b68d24015df727d3d",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "ea9b68d24015df727d3d",
+    "url": "/static/js/2.07f7f80f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07f7f80f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9889b5c04f821c33fd74",
+    "url": "/static/js/main.ee845ae5.chunk.js"
+  },
+  {
+    "revision": "0d305b1a9ef39e85fa0b",
+    "url": "/static/js/runtime-main.3ff2916f.js"
+  }
+]);
